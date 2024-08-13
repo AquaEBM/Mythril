37b162312c0d91aafa4448b053d64df71f2fc3c0
AquaEBM <AquaEBM@gmail.com>
2024-08-13 21:29:48 +0200

    bump deps and, change feature name

--- simd_util/src/lib.rs
+++ simd_util/src/lib.rs
@@ -2,28 +2,22 @@
 
 use cfg_if::cfg_if;
 
-cfg_if! {
-
-    if #[cfg(feature = "non_std_simd")] {
-
-        pub mod simd {
-            pub use core_simd::simd::*;
-            pub use std_float::*;
-        }
-
-    } else if #[cfg(feature = "std_simd")] {
-        pub use std::simd;
-    }
+#[cfg(feature = "core_simd_crate")]
+pub mod simd {
+    pub use core_simd::simd::*;
+    pub use std_float::*;
 }
+#[cfg(feature = "std_simd")]
+pub use std::simd;
 
 cfg_if! {
-    if #[cfg(any(feature = "std_simd", feature = "non_std_simd"))] {
+    if #[cfg(any(feature = "std_simd", feature = "core_simd_crate"))] {
 
-pub mod filter;
-pub mod math;
-pub mod smoothing;
-mod util;
-pub use util::*;
+        pub mod filter;
+        pub mod math;
+        pub mod smoothing;
+        mod util;
+        pub use util::*;
 
     }
 }
